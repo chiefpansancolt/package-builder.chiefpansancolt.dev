3331d4fd512eabced66f4ed7ac0c988374cf6c70
Christopher Pezza <chris.pezza@hey.com>
2023-03-28 15:30:11 -0400

    add navigation data

---
+++ src/data/navigation.js
@@ -0,0 +1,29 @@
+const navigation = [
+  {
+    title: "Introduction",
+    links: [
+      { title: "Getting started", href: "/" },
+      { title: "Installation", href: "/docs/installation" },
+    ],
+  },
+  {
+    title: "Contributing",
+    links: [
+      { title: "How to contribute", href: "/docs/how-to-contribute" },
+      { title: "Sponsor", href: "/docs/sponsor" },
+      { title: "Change log", href: "/docs/change-log" },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      { title: "Roadmap", href: "https://github.com/users/chiefpansancolt/projects/7" },
+      {
+        title: "License",
+        href: "https://github.com/chiefpansancolt/package-builder/blob/main/LICENSE",
+      },
+    ],
+  },
+]
+
+export default navigation
