2cb8b02a1ec9763ed635c65ce9210d21ca9f4ab4
Christopher Pezza <chris.pezza@hey.com>
2023-03-29 13:21:00 -0400

    remove theme button

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -56,7 +56,7 @@ function Header({ navigation }) {
         <Search />
       </div>
       <div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
-        <ThemeSelector className="relative z-10" />
+        {/* <ThemeSelector className="relative z-10" /> */}
         <Link href="https://github.com/chiefpansancolt/package-builder" className="group" aria-label="GitHub">
           <GitHubIcon className="h-6 w-6 fill-slate-400 group-hover:fill-slate-500 dark:group-hover:fill-slate-300" />
         </Link>
